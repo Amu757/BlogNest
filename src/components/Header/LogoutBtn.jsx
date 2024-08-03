b2efb0f93c3764ccd5c4386f8565d16ff09b0f6d
Aman Waghmare <amanwaghmare311@gmail.com>
2024-08-03 19:13:15 +0530

    corrections

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,13 +1,16 @@
 import { useDispatch } from "react-redux";
 import authService from "../../appwrite/auth";
 import { logout } from "../../store/authSlice";
+import { useNavigate } from "react-router-dom";
 
 function LogoutBtn() {
+  const navigate = useNavigate()
   const dispatch = useDispatch();
   const logoutHandler = () => {
     authService.logout().then(() => {
       dispatch(logout());
     });
+     navigate("/")
   };
   return <button className="navButton logoutBtn" onClick={logoutHandler}>LogOut</button>;
 }
